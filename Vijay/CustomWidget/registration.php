9123b7a6a4ed87a1c46320722b84bd1a663dd081
Vijay Rami <vijay.rami@iflair.com>
2020-07-22 15:57:31 +0530

    First Commit

---
+++ Vijay/CustomWidget/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+	ComponentRegistrar::MODULE,
+	'Vijay_CustomWidget',
+	__DIR__
+);
+
